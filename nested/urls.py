961420c8cfba35d4fb074be3ca7d1e98c7b2defc
Al Momin Faruk <31950303+faruk6276@users.noreply.github.com>
2021-06-07 16:02:05 +0600

    Filtering Url

--- nested/urls.py
+++ nested/urls.py
@@ -1,12 +1,15 @@
-from django.urls import path,include
+from django.urls import path,include,re_path
 from . import views
 from rest_framework.routers import DefaultRouter
 
 
-routers=DefaultRouter()
-routers.register('address',views.AddressView,basename='country')
+# routers=DefaultRouter()
+# routers.register('address',views.AddressView,basename='country')
 
 
 urlpatterns = [
-     path('api/',include(routers.urls)),
+     # path('api/',include(routers.urls)),
+     #path('api/country/',views.CountryViewList.as_view(),name="countrylist"),
+     path('api/country/<int:pk>',views.CountryViewDetail.as_view(),name="countrydetails"),
+     path('api/country/',views.CountryList.as_view()),
 ]
--- nested/views.py
+++ nested/views.py
@@ -4,9 +4,65 @@ from .models import *
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework import viewsets
+from rest_framework import viewsets,generics
+from django.http import Http404
+from django_filters.rest_framework import DjangoFilterBackend
 # Create your views here.
 
-class AddressView(viewsets.ModelViewSet):
+# class AddressView(viewsets.ModelViewSet):
+#     queryset = Country.objects.all()
+#     serializer_class = CountrySerializer
+
+class CountryList(generics.ListAPIView):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('country_name',)
+    
+    # print("COUTRYLIST")
+    # def get_queryset(self):
+    #     country_name = self.kwargs['country_name']
+    #     print('okkkkkkkkk')
+    #     print(country_name)
+    #     return Country.objects.filter(country_name=country_name)
+class CountryViewList(APIView):
+
+    def get(self, request, format=None):
+        snippets = Country.objects.all()
+        serializer = CountrySerializer(snippets, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = CountrySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CountryViewDetail(APIView):
+    """
+    Retrieve, update or delete a snippet instance.
+    """
+    def get_object(self, pk):
+        try:
+            return Country.objects.get(pk=pk)
+        except Country.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = CountrySerializer(snippet)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = CountrySerializer(snippet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
